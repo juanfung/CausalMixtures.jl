7f789f1593f5f2364f893058fd2c86946086d738
Juan F. Fung <juan.fung@nist.gov>
2025-07-18 13:47:48 -0400

    Update ppd calculations to use correct weights (just component probability vs prob x likelihood)

--- src/dpm_ppd.jl
+++ src/dpm_ppd.jl
@@ -3,7 +3,7 @@ export dpm_ppd, rand_ppd, rand_dpm, rand_blocked, rand_gaussian, dpm_ate
 
 ## --------------------------------------------------------------------------- #
 ## TODO: dpm_ppd computes posterior predictive density values
-function dpm_ppd(out::GibbsOut, znew::Vector{Float64}, prior_theta::PriorTheta;
+function dmp_ppd(out::GibbsOut, znew::Vector{Float64}, prior_theta::PriorTheta;
                  ##hnew::SparseMatrixCSC{Float64,Int64}
                  xid::UnitRange=1:(length(znew)-1))
     
@@ -19,7 +19,7 @@ function dpm_ppd(out::GibbsOut, znew::Vector{Float64}, prior_theta::PriorTheta;
     hnew = blockdiag(sparse(znew'), sparse(znew[xid]'), sparse(znew[xid]')) # 3 x ktot
     
     ## pre-allocate storage
-    ynew_out = Array{Float64}(undef, 0) #Array{Float64}(undef, 3M)
+    ynew_out = Array{Float64}(undef, 0)
     ynew = Array{Float64}(undef, 0)
     w = Array{Float64}(undef, 0)
     
@@ -27,14 +27,13 @@ function dpm_ppd(out::GibbsOut, znew::Vector{Float64}, prior_theta::PriorTheta;
         
         alpha = out.out_dp[m].alpha
         J = out.out_dp[m].J
-        ##labels = out.out_dp[m].labels
         njs = out.out_dp[m].njs
         theta = out.out_theta[m]
         
         for j in 1:J
             hb = hnew*theta[j].beta
             append!( ynew, hb + cholesky(theta[j].Sigma).U'*randn(3) )
-            push!( w, njs[j]*exp(logpdf(MvNormal(hb, theta[j].Sigma), ynew[(3*(j-1)+1):3*j])) )
+            push!( w, njs[j] )
         end
         
         ## draw from prior
@@ -42,15 +41,10 @@ function dpm_ppd(out::GibbsOut, znew::Vector{Float64}, prior_theta::PriorTheta;
         beta = mu + cholesky(V).U'*randn(length(mu))
         hb = hnew*beta
         append!( ynew, hb + cholesky(Sigma).U'*randn(3) )
-        push!( w, alpha*exp(logpdf(MvNormal(hb, Sigma), ynew[(3*J+1):3*(J+1)])) )
+        push!( w, alpha )
         
-        ##jm = findmax(w)[2]
         jm = rand( Distributions.Categorical(w/sum(w)) )
         append!( ynew_out, ynew[(3*(jm-1)+1):3*jm] )
-        
-        ##ynew = reshape(ynew, 3, J+1)
-        ##append!( ynew_out, ynew[:, findmax(w)[2] ] )
-        ##ynew_out[:,m] = ynew[ findmax(w)[2] ]
         
         ## reset storage
         resize!(ynew, 0)
@@ -74,7 +68,7 @@ function rand_dpm(out::GibbsOut, p::PriorTheta, hnew::SparseMatrixCSC{Float64,In
         V = inv(p.prior_beta.V)
     end
     ## pre-allocate storage
-    ynew_out = Array{Float64}(undef, 0) #Array{Float64}(undef, 3M)
+    ynew_out = Array{Float64}(undef, 0)
     ynew = Array{Float64}(undef, 0)
     w = Array{Float64}(undef, 0)    
     for m in 1:length(out.out_dp)        
@@ -85,16 +79,15 @@ function rand_dpm(out::GibbsOut, p::PriorTheta, hnew::SparseMatrixCSC{Float64,In
         for j in 1:J
             hb = hnew*theta[j].beta
             append!( ynew, hb + cholesky(theta[j].Sigma).U'*randn(3) )
-            push!( w, njs[j]*exp(logpdf(MvNormal(hb, theta[j].Sigma), ynew[(3*(j-1)+1):3*j])) )
+            push!( w, njs[j] )
         end        
         ## draw from prior
         Sigma = NobileWishart(rho, rhoR)
         beta = mu + cholesky(V).U'*randn(length(mu))
         hb = hnew*beta
         append!( ynew, hb + cholesky(Sigma).U'*randn(3) )
-        push!( w, alpha*exp(logpdf(MvNormal(hb, Sigma), ynew[(3*J+1):3*(J+1)])) )
+        push!( w, alpha )
         ## select component
-        ##jm = findmax(w)[2]
         jm = rand( Distributions.Categorical(w/sum(w)) )
         append!( ynew_out, ynew[(3*(jm-1)+1):3*jm] )
         ## reset storage
@@ -107,7 +100,7 @@ end
 function rand_blocked(out::GibbsOut, hnew::SparseMatrixCSC{Float64,Int64}) # also works for fmn
     ## setup...
     ## pre-allocate storage
-    ynew_out = Array{Float64}(undef, 0) #Array{Float64}(undef, 3M)
+    ynew_out = Array{Float64}(undef, 0)
     ynew = Array{Float64}(undef, 0)
     w = Array{Float64}(undef, 0)
     for m in 1:length(out.out_dp)
@@ -117,9 +110,8 @@ function rand_blocked(out::GibbsOut, hnew::SparseMatrixCSC{Float64,Int64}) # als
             wj = ws[j].w
             hb = hnew * theta[j].beta
             append!( ynew, hb + cholesky(theta[j].Sigma).U'*randn(3) )
-            push!( w, wj*exp(logpdf(MvNormal(hb, theta[j].Sigma), ynew[(3*(j-1)+1):3*j])) )
+            push!( w, wj )
         end
-        ##jm = findmax(w)[2]
         jm = rand( Distributions.Categorical(w/sum(w)) )
         append!( ynew_out, ynew[(3*(jm-1)+1):3*jm] )
         ## reset storage
@@ -129,12 +121,12 @@ function rand_blocked(out::GibbsOut, hnew::SparseMatrixCSC{Float64,Int64}) # als
     return reshape(ynew_out, 3, length(out.out_dp)) 
 end
 
-function rand_fmn(out::GibbsOut, input::GibbsInput, hnew::SparseMatrixCSC{Float64,Int64}) # also works for fmn
+function rand_fmn(out::GibbsOut, input::GibbsInput, hnew::SparseMatrixCSC{Float64,Int64})
     ## setup...
     J = input.priors.prior_dp.J
     aJ = input.priors.prior_dp.alpha/float(J)
     ## pre-allocate storage
-    ynew_out = Array{Float64}(undef, 0) #Array{Float64}(undef, 3M)
+    ynew_out = Array{Float64}(undef, 0)
     ynew = Array{Float64}(undef, 0)
     w = Array{Float64}(undef, 0)
     for m in 1:length(out.out_dp)
@@ -143,9 +135,8 @@ function rand_fmn(out::GibbsOut, input::GibbsInput, hnew::SparseMatrixCSC{Float6
         for j in 1:J
             hb = hnew * theta[j].beta
             append!( ynew, hb + cholesky(theta[j].Sigma).U'*randn(3) )
-            push!( w, (njs[j] + aJ)*exp(logpdf(MvNormal(hb, theta[j].Sigma), ynew[(3*(j-1)+1):3*j])) )
+            push!( w, njs[j] + aJ )
         end
-        ##jm = findmax(w)[2]
         jm = rand( Distributions.Categorical(w/sum(w)) )
         append!( ynew_out, ynew[(3*(jm-1)+1):3*jm] )
         ## reset storage
@@ -158,16 +149,13 @@ end
 function rand_gaussian(out::GibbsOut, hnew::SparseMatrixCSC{Float64,Int64})
     ## setup...
     ## pre-allocate storage
-    ynew_out = Array{Float64}(undef, 0) #Array{Float64}(undef, 3M)
+    ynew_out = Array{Float64}(undef, 0)
     ynew = Array{Float64}(undef, 0)
-    #w = Array{Float64}(undef, 0)
     for m in 1:length(out.out_dp)
         hb = hnew * out.out_theta[m][1].beta
         append!( ynew_out, hb + cholesky(out.out_theta[m][1].Sigma).U'*randn(3) )
-        #push!( w, exp(logpdf(MvNormal(hb, out.out_theta[m][1].Sigma), ynew[(3*(j-1)+1):3*j])) )
         ## reset storage
         resize!(ynew, 0)
-        #resize!(w, 0)
     end
     return reshape(ynew_out, 3, length(out.out_dp))
 end
@@ -182,7 +170,6 @@ function rand_ppd(out::GibbsOut, input::GibbsInput, znew::Vector{Float64})
         ppd = rand_fmn(out, input, hnew)
     elseif input.params.model == "gaussian"
         ppd = rand_gaussian(out, hnew)
-    ##elseif in(input.params.model, ["blocked", "fmn"])        
     else
         error("No method for model $(input.params.model)!")
     end
@@ -213,15 +200,13 @@ function dpm_density(ynew::Matrix{Float64}, input::GibbsInput)
     ttNew = ateNew[idx]
     ## PPD(grid, ate.dens, tt.dens)
     
-    return PPD(grid=linspace(1,1,1), ate=ateNew, tt=ttNew)
+    return PPD(grid=range(1, 1, length=1), ate=ateNew, tt=ttNew)
 
 end
 
 ## --------------------------------------------------------------------------- #
 ## output active components
-function out_J(out::GibbsOut) #, input::GibbsInput)
-    ##model = input.params.model
-    ##if !in(model, ["dpm", "blocked"]) error("Model is not a DP Mixture!") end
+function out_J(out::GibbsOut)
     outJ = Array{Int64}(undef, length(out.out_dp))
     for m in 1:length(out.out_dp)
         outJ[m] = out.out_dp[m].J
